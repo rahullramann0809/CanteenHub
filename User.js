ef990cc9cb8cbf48cbe5f151caac1adebe7b461d
rahullramann0809 <rahulraman0809@gmail.com>
2026-04-17 10:22:31 +0530

    Add User.js model with authentication

---
+++ User.js
@@ -0,0 +1,19 @@
+// User.js Model with authentication
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+module.exports = mongoose.model('User', userSchema);
